40ac1ec5253e16b1185febd683d3b8d8b4d37ce4
12bng <12bng2@gmail.com>
2019-09-11 14:26:25 +0200

    faltaba el private chat

--- src/accesoADatos/PrivateChat.java
+++ src/accesoADatos/PrivateChat.java
@@ -1,5 +1,23 @@
 package accesoADatos;
 
-public class PrivateChat {
+import java.util.ArrayList;
 
+import modelo.Message;
+import modelo.User;
+
+public class PrivateChat {
+	User user1, user2;
+	ArrayList<Message> menssages;
+	
+	public PrivateChat() {
+		menssages = new ArrayList<Message>();
+		
+	}
+	
+	public boolean isSameChat(User user1, User user2) {
+		if((user1.getId()==this.user1.getId()||user2.getId()==this.user1.getId())&&(user1.getId()==this.user2.getId()||user2.getId()==this.user2.getId())) {
+			return true;
+		}
+		return false;
+	}
 }
